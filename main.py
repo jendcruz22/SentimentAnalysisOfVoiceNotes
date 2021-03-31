d5965a055232f248a78c5132de45ac9ba35b61c2
jendcruz22 <jendcruz23@gmail.com>
2021-03-31 11:01:56 +0530

    completed : web design

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import os
-import sys
 from werkzeug.utils import secure_filename
-from flask import Flask, flash, request, redirect, send_file, render_template
-from flask_bootstrap import Bootstrap
-import shutil
+from flask import Flask,flash,request,redirect,send_file,render_template
 
 UPLOAD_FOLDER = 'uploads/'
 
@@ -15,45 +12,57 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def index():
     return render_template('index.html')
 
-# Sentiment Analysis
-@app.route('/sentimentanalysis', methods=['GET', 'POST'])
+# Upload API
+@app.route('/uploadfile', methods=['GET', 'POST'])
 def sentimentanalysis():
 
+    # Delete existing files from the upload before processing the next/new input audio
+    for filename in os.listdir(UPLOAD_FOLDER):
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        
+        try:
+            if filename!=".gitkeep":
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+
+        except Exception as e:
+            print('Failed to delete. Reason: %s' % (e))
+    
     if request.method == 'POST':
 
-        # Delete existing files from the upload, download, and audio folders before processing the next/new input audio
-        for filename in os.listdir(UPLOAD_FOLDER) or filename:
-            file_path = os.path.join(UPLOAD_FOLDER, filename)
-            try:
-                if filename!=".gitkeep" in UPLOAD_FOLDER:
-                    if os.path.isfile(file_path) or os.path.islink(file_path):
-                        os.unlink(file_path)
-
-                    elif os.path.isdir(file_path):
-                        shutil.rmtree(file_path)
-
-            except Exception as e:
-                print('Failed to delete. Reason: %s' % (e))
-    
         # check if the post request has the file part
         if 'file' not in request.files:
             print('no file')
             return redirect(request.url)
         file = request.files['file']
 
-        # if user does not select file, browser also submit a empty part without filename
+        # if user does not select file, browser also
+        # submit a empty part without filename
         if file.filename == '':
             print('no filename')
             return redirect(request.url)
-
         else:
             filename = secure_filename(file.filename)
-            # save the input audio in the uploads folder
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             print("saved file successfully")
-        
-        # return redirect('sentimentanalysis.html')
+
+      #send file name as parameter to downlad
+            return redirect('/downloadfile/'+ filename)
     return render_template('sentimentanalysis.html')
+
+
+# Download API
+@app.route("/downloadfile/<filename>", methods = ['GET'])
+def download_file(filename):
+    return render_template('download.html',value=filename)
+
+@app.route('/return-files/<filename>')
+def return_files_tut(filename):
+    file_path = UPLOAD_FOLDER + filename
+    return send_file(file_path, as_attachment=True, attachment_filename='')
 
 if __name__ == "__main__":
     app.run(debug=True)
