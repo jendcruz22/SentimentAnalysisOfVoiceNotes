a65697c3ececd2293da32a0ee7851ec5e18740c9
jendcruz22 <jendcruz23@gmail.com>
2021-04-02 18:17:57 +0530

    modified : added dynamic emotion and audio display

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import os
 import glob
 from werkzeug.utils import secure_filename
 from flask import Flask,flash,request,redirect,send_file,render_template
+import shutil
 
 UPLOAD_FOLDER = 'uploads/'
+AUDIO_FOLDER = './static/audios/'
 
 app = Flask(__name__, template_folder='templates')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-headings = ["Audio segment", "Predicted emotion"]
-data = [["blah1", "blah1"], ["blah2", "blah2"], ["blah3", "blah3"], ["blah4", "blah4"], ["blah5", "blah5"], ["blah6", "blah6"]]
-arr = os.listdir('./uploads')
+data=[]
+emotion=["angry"]
 
 # Home page
 @app.route('/')
@@ -21,20 +21,22 @@ def index():
 @app.route('/uploadfile', methods=['GET', 'POST'])
 def sentimentanalysis():
 
-    # Delete existing files from the upload before processing the next/new input audio
-    # for filename in os.listdir(UPLOAD_FOLDER):
-    #     file_path = os.path.join(UPLOAD_FOLDER, filename)
-        
-    #     try:
-    #         if filename!=".gitkeep":
-    #             if os.path.isfile(file_path) or os.path.islink(file_path):
-    #                 os.unlink(file_path)
+    # Delete existing files from the upload, download, and audio folders before processing the next/new input audio
+    for filename in os.listdir(UPLOAD_FOLDER) or filename in os.listdir(AUDIO_FOLDER):
+        file_path1 = os.path.join(UPLOAD_FOLDER, filename)
+        file_path2 = os.path.join(AUDIO_FOLDER, filename)
+        try:
+            if filename!=".gitkeep":
+                if os.path.isfile(file_path1) or os.path.islink(file_path1) or os.path.isfile(file_path2) or os.path.islink(file_path2):
+                    os.unlink(file_path1)
+                    os.unlink(file_path2)
 
-    #             elif os.path.isdir(file_path):
-    #                 shutil.rmtree(file_path)
+                elif os.path.isdir(file_path1) or os.path.isdir(file_path2):
+                    shutil.rmtree(file_path1)
+                    shutil.rmtree(file_path2)
 
-    #     except Exception as e:
-    #         print('Failed to delete. Reason: %s' % (e))
+        except Exception as e:
+            print('Failed to delete. Reason: %s' % (e))
     
     if request.method == 'POST':
 
@@ -52,7 +54,7 @@ def sentimentanalysis():
         else:
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            print("saved file successfully")
+            shutil.copy(os.path.join(app.config['UPLOAD_FOLDER'], filename), os.path.join(app.config['AUDIO_FOLDER'], filename))
 
       #send file name as parameter to downlad
             return redirect('/downloadfile/'+ filename)
@@ -62,18 +64,11 @@ def sentimentanalysis():
 # Download API
 @app.route("/downloadfile/<filename>", methods = ['GET'])
 def download_file(filename):
-    for i in arr:
-        if i!='.gitkeep':
-            # print(i)
-        
-            for j, i in enumerate(glob.glob(".\\uploads\\*.mp3")):
-                data[j][0] = i
-    return render_template('download.html',value=filename, headings=headings, data=data)
-
-# @app.route('/return-files/<filename>')
-# def return_files_tut(filename):
-#     file_path = UPLOAD_FOLDER + filename
-#     return send_file(file_path, as_attachment=True, attachment_filename='')
+    for audio_files in AUDIO_FOLDER:
+        data=glob.glob('./static/audios/*.mp3', recursive=True)
+    result = [sub.replace('./static/', '') for sub in data]
+    res = [sub.replace('\\', '/') for sub in result]
+    return render_template('download.html',data=data, res=res, emotion=emotion, len=len(emotion))
 
 if __name__ == "__main__":
     app.run(debug=True)
